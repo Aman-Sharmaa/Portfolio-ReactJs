55b73f93e87a2b9ffde07c70dd1aee0fb85a4ea0
Aman Sharma <52895888+Aman-Sharmaa@users.noreply.github.com>
2023-08-23 01:25:14 +0530

    build

--- src/screens/NewTemplate/index.js
+++ src/screens/NewTemplate/index.js
@@ -7,7 +7,7 @@ import { useNavigate, Link } from "react-router-dom";
 import Aman from "../../assets/img/aman.svg";
 import BL from "../../assets/img/bl.svg";
 import EASY from "../../assets/img/easyexport.svg";
-import Esy from "../../assets/img/easybil.svg";
+import Esy from "../../assets/img/easy.png";
 import Ped from "../../assets/img/ped.png";
 import Floro from "../../assets/img/florosense.svg";
 import Aikyaa from "../../assets/img/aikya.svg";
@@ -29,16 +29,21 @@ function NewTemplate() {
           <div className="name-profile-left">
             <h2>I'm Aman Sharma</h2>
             <p>
-              Just Doing what I Like, <br />
-              Working at{" "}
+              Transforming ideas into reality!
+              <br />
+              Currently Working at
               <a
                 href="https://www.linkedin.com/company/mykids-ventures-private-limited/"
                 target="_blank"
               >
                 BeingTheParent
               </a>
+              <p>
+                Drop a ❤️ on
+                <a href="mailto:amnshrmaa@gmail.com">hi@amansharma.co</a>
+              </p>
               <div className="btn">
-                <a href="mailto:amansharma57269@gmail.com">
+                <a href="tel:+91 9506521413">
                   <button id="darkBtn">
                     <AiOutlinePlusCircle id="darkIcon" />
                     Hire Me
@@ -50,7 +55,7 @@ function NewTemplate() {
                   target="_blank"
                 >
                   <button id="lightBtn">
-                    <FaHandPaper id="darkIcon" />
+                    <AiOutlineCopy id="darkIcon" />
                     Connect
                   </button>
                 </a>
@@ -65,10 +70,10 @@ function NewTemplate() {
           <div className="userName-active">
             <h1>
               <span id="active" />
-              Projects
+              Projects (R&D)
             </h1>
           </div>
-
+          {/*
           <a href="https://btpbrandlabs.com/" target="_blank">
             <div className="ListOfProjects">
               <div className="projectImg">
@@ -79,57 +84,48 @@ function NewTemplate() {
                 <p id="overview">Business Model, MERN Stack</p>
               </div>
               <div className="projectView">
-                <GrFormNext id="next" />
+
               </div>
             </div>
-          </a>
+          </a> */}
 
-          <a href="https://easyexport.in/" target="_blank">
+          <a href="https://bit.ly/esyxprt" target="_blank">
             <div className="ListOfProjects">
               <div className="projectImg">
                 <img src={EASY} />
               </div>
               <div className="projectName">
                 <p id="name">EasyExport</p>
-                <p id="overview">Import Export Billing Automation, MERN</p>
+                <p id="overview">India-Bhutan Import Export Billing System </p>
               </div>
-              <div className="projectView">
-                <GrFormNext id="next" />
-              </div>
+              <div className="projectView">{/*  */}</div>
             </div>
           </a>
-
-          <a href="https://www.florosense.com/" target="_blank">
+          <a href="https://bit.ly/floro" target="_blank">
             <div className="ListOfProjects">
               <div className="projectImg">
                 <img src={Floro} />
               </div>
               <div className="projectName">
                 <p id="name">Florosense</p>
-                <p id="overview">Products Showcase, Wordpress</p>
+                <p id="overview">Energy & Environment Solutions</p>
               </div>
-              <div className="projectView">
-                <GrFormNext id="next" />
-              </div>
+              <div className="projectView"></div>
             </div>
           </a>
-
-          <a href="https://aikyaa.in/" target="_blank">
+          <a href="https://bit.ly/aikyaa" target="_blank">
             <div className="ListOfProjects">
               <div className="projectImg">
                 <img src={Aikyaa} />
               </div>
               <div className="projectName">
                 <p id="name">Aikyaa</p>
-                <p id="overview">CA Website, Wordpress</p>
+                <p id="overview">CA Consultation</p>
               </div>
-              <div className="projectView">
-                <GrFormNext id="next" />
-              </div>
+              <div className="projectView"></div>
             </div>
           </a>
-
-          <a href="https://indiecherry.com/" target="_blank">
+          {/* <a href="https://indiecherry.com/" target="_blank">
             <div className="ListOfProjects">
               <div className="projectImg">
                 <img src={Indie} />
@@ -138,28 +134,22 @@ function NewTemplate() {
                 <p id="name">IndieCherry</p>
                 <p id="overview">Marketplace, WooCommerce</p>
               </div>
-              <div className="projectView">
-                <GrFormNext id="next" />
-              </div>
+              <div className="projectView"></div>
             </div>
-          </a>
-
-          <a href="https://bill.easyexport.in" target="_blank">
+          </a> */}
+          <a href="https://bit.ly/esyxprt" target="_blank">
             <div className="ListOfProjects">
               <div className="projectImg">
                 <img src={Esy} />
               </div>
               <div className="projectName">
                 <p id="name">Easy Bill</p>
-                <p id="overview">Marketplace, WooCommerce</p>
+                <p id="overview">Billing System</p>
               </div>
-              <div className="projectView">
-                <GrFormNext id="next" />
-              </div>
+              <div className="projectView"></div>
             </div>
           </a>
-
-          <a href="https://www.pediatricimmunologist.in/" target="_blank">
+          {/* <a href="https://www.pediatricimmunologist.in/" target="_blank">
             <div className="ListOfProjects">
               <div className="projectImg">
                 <img src={Ped} />
@@ -168,12 +158,9 @@ function NewTemplate() {
                 <p id="name">Pediatric Immunologist</p>
                 <p id="overview">Doctor Consultation, PHP </p>
               </div>
-              <div className="projectView">
-                <GrFormNext id="next" />
-              </div>
+              <div className="projectView"></div>
             </div>
           </a>
-
           <a href="http://www.drstalin.in/" target="_blank">
             <div className="ListOfProjects">
               <div className="projectImg">
@@ -183,13 +170,10 @@ function NewTemplate() {
                 <p id="name">Dr Stalin</p>
                 <p id="overview">Doctor Consultation, PHP </p>
               </div>
-              <div className="projectView">
-                <GrFormNext id="next" />
-              </div>
+              <div className="projectView"></div>
             </div>
-          </a>
-
-          <a href="http://www.multigrad.in/" target="_blank">
+          </a> */}
+          <a href="https://bit.ly/mgrnd" target="_blank">
             <div className="ListOfProjects">
               <div className="projectImg">
                 <img src={MG} />
@@ -198,40 +182,47 @@ function NewTemplate() {
                 <p id="name">Multigrad</p>
                 <p id="overview">Edtech, React Native, MERN </p>
               </div>
-              <div className="projectView">
-                <GrFormNext id="next" />
-              </div>
+              <div className="projectView"></div>
             </div>
           </a>
         </div>
+        <div className="projects">
+          <div className="userName-active">
+            <h1>
+              <span id="active" />
+              Skills
+            </h1>
+          </div>
+          <div className="skills">
+            <p id="type">Backend Technology</p>
+            <p>ExpressJs, NodeJs, PHP</p>
+            <p id="type">FrontEnd Technology</p>
+            <p>
+              ReactJs, HTML5, CSS3, BootStrap, JS, Wordpress(Custom
+              Developement), ElectronJs
+            </p>
+            <p id="type">Tools & Technology</p>
+            <p>Figma, Github, Linux</p>
+            <p id="type">App Developement</p>
+            <p>React Native</p>
+            <p>
+              <a href="https://bit.ly/amnshrma" target="_blank">
+                Download Resume
+              </a>
+            </p>
+          </div>
+        </div>
         <div className="connect">
           <h3>Let's work together</h3>
-          <p>Creating user experience and visual appealing design</p>
-          <div className="btnCnt">
-            <a href="mailto:amansharma57269@gmail.com">
-              <button id="darkBtn">
-                <AiOutlinePlusCircle id="darkIcon" />
-                Hire Me
-              </button>
-            </a>
-
-            <a
-              href="https://calendly.com/amnshrma/30min?back=1&month=2023-08"
-              target="_blank"
-            >
-              <button id="lightBtn">
-                <FaHandPaper id="darkIcon" />
-                Connect
-              </button>
-            </a>
-          </div>
+          <p>Transforming ideas into reality!</p>
+          <div className="btnCnt"></div>
         </div>
 
         {/* <div className="social">
           <div className="userName-active">
             <h1>
               <span id="active" />
-              Follow Me
+              Resume
             </h1>
           </div>
           <div className="socialRight"></div>
